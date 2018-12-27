b39887ed0f036206d8dcb628708c31155e079d18
kosemMG <gelberg@protonmail.com>
2018-12-27 16:11:14 +0200

    task 4

--- index.php
+++ index.php
@@ -55,3 +55,41 @@ $milk = new Product(2,'Milk', 1.95, date("Y.m.d", time() + (5 * 24 * 3600)));
 
 $bread->displayProduct();
 $milk->displayProduct();
+
+// Task 4
+
+class Clothes extends Product
+{
+    protected $color;
+    protected $size;
+
+    public function __construct($id, $name, $color, $size, $price)
+    {
+        parent::__construct($id, $name, $price);
+        $this->color = $color;
+        $this->size = $size;
+    }
+
+    public function displayProduct()
+    {
+        echo "<h3>{$this->name}</h3>";
+        echo "<p>The color is: {$this->color}</p>";
+        echo "<p>The size is: {$this->size}</p>";
+        echo "<p>The price is: &#36;{$this->price}</p>";
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+}
+
+$shirt = new Clothes(3,'Shirt', 'white', 'XL', 29.99);
+
+$shirt->displayProduct();
+
