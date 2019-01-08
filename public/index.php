6e6bda7236adb12dbe7eb5a3f9a0ca8bdc1bc1a3
kosemMG <gelberg@protonmail.com>
2019-01-09 00:11:30 +0200

    Modified: class Autoloader

--- public/index.php
+++ public/index.php
@@ -8,12 +8,12 @@ use \app\services\Autoloader;
 spl_autoload_register([new Autoloader(), 'loadClass']);
 
 
-$product = \app\models\Product::getOne(7);
+$product = \app\models\Product::getOne(3);
 //$product = new \app\models\Product();
 var_dump($product);
 
 //$product->properties['name'] = 'name1';
 //$product->properties['price'] = 10;
-$product->properties['description'] = 'desc1';
+//$product->properties['description'] = 'desc1';
 
-$product->delete();
+//$product->delete();
